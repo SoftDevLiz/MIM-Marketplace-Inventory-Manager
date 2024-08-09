48e475cf462364add60f057474d3235ed73b139d
Liezl Mouton <liz.mouton1@gmail.com>
2024-08-09 16:42:09 +0200

    Creates a preload to bridge the gap between the renderer and the main

---
+++ renderer.js
@@ -0,0 +1,3 @@
+const preloadPractice = document.getElementById('preprac')
+
+preloadPractice.innerText = `This is my preload practice. We are using Chrome ${versions.chrome()}, Node.js ${versions.node()}, and Electron ${versions.electron()}`
