dd67ff876fef18e89e9e8041a20825dafe97be55
Liezl Mouton <liz.mouton1@gmail.com>
2024-08-12 17:41:45 +0200

    Rewrites main script from JS to TS

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-// Main.js controls the app window and app window lifecycle
+// Main.ts controls the app window and app window lifecycle
+import { app, BrowserWindow } from 'electron'
+import * as path from 'node:path'
 
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
-
-const createWindow = () => {
-    const window = new BrowserWindow({
+const createWindow = (): void => {
+    const window: BrowserWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -16,15 +15,15 @@ const createWindow = () => {
 }
 
 // Electron can only create an app window after the ready event is fired. The below listens for it.
-app.whenReady().then(() => {
+app.whenReady().then((): void => {
     createWindow()
 // MacOS apps run even when the windows are closed. The below calls createWindow when no app windows are found to be open.
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    app.on('activate', (): void => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
     })
 })
 
 // Listens for 'window-all-closed' event & quits the app if the user is not on MacOS (darwin)
-app.on('window-all-closed', () => {
+app.on('window-all-closed', (): void => {
     if (process.platform !== 'darwin') app.quit()
 })
