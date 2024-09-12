1184ae80ca98bdd2815f9d04de8b7078a165ead4
Liezl Mouton <liz.mouton1@gmail.com>
2024-09-12 16:03:07 +0200

    Fresh start with electron-vite

--- README.md
+++
@@ -1,2 +0,0 @@
-# MIM-Marketplace-Inventory-Manager
-Stock QTY scanning doohickey for marketplaces. Scan your stock -> MIM will update your local spreadsheet QTY column-> Import your spreadsheet to the marketplace.
--- src/renderer.ts
+++
@@ -1,9 +0,0 @@
-// renderer.ts is responsible for rendering web content.
-
-console.log("Hi I'm defs connected and running");
-
-const preloadPractice: HTMLElement | null = document.getElementById('preprac')
-
-if (preloadPractice) {
-    preloadPractice.innerText = `This is my preload practice. We are using Chrome ${window.versions.chrome()}, Node.js ${window.versions.node()}, and Electron ${window.versions.electron()}` 
-}
