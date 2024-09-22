e274afa0be92e1feebefb71368cbc28327affbf7
Liezl Mouton <liz.mouton1@gmail.com>
2024-09-22 19:49:57 +0200

    Starting over (again)

--- MIM-Marketplace-Inventory-Manager/src/renderer/src/components/TestComponent.jsx
+++
@@ -1,9 +0,0 @@
-function TestComponent() {
-    return (
-        <div>
-            <h1 className="test">Do I still remember how to React?</h1>
-        </div>
-    )
-} 
-
-export default TestComponent;
