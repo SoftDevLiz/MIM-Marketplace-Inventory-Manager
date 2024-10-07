91864f42ba694097ce4626968e05eb88a199c03e
Liezl Mouton <liz.mouton1@gmail.com>
2024-10-07 17:23:11 +0200

    Change component state

--- MIM/src/renderer/src/components/StockSheetSelector.jsx
+++ MIM/src/renderer/src/components/StockSheetSelector.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react'
-
+import * as XLSX from 'xlsx'
 
 function StockSheetSelector() {
-    const [file, setFile] = useState("")
+    const [workbookData, setWorkbookData] = useState(null)
 
+    // imports excel file from file system
     const importFile = async () => {
         try {
-          const filePaths = await window.fileSystem.openFileDialog();
-          console.log(filePaths); 
+            const filePaths = await window.fileSystem.openFileDialog();
+      
+            const noFileSelected = filePaths.length === 0;
+      
+            if (noFileSelected) {
+              throw new Error("No file selected");
+            }
+      
+            const xlFile = filePaths[0];
+            setFile(xlFile);
+      
+            const workbook = XLSX.read(file);
+            console.log(workbook)
+            const sheetName = workbook.SheetNames[0];
+            console.log(sheetName)
+            const sheet = workbook.Sheets[sheetName]
+            console.log(sheet)
+            const data = XLSX.utils.sheet_to_json(sheet)
+      
+            console.log(data);
         } catch (error) {
-          console.error("Failed to open file dialog:", error);
+          console.error("Error importing file:", error);
         }
       };
 
@@ -20,7 +39,6 @@ function StockSheetSelector() {
             type="button"
             onClick={importFile}
         >Browse local files</button>
-        <h2>{file}</h2>
         </>
     )
 
